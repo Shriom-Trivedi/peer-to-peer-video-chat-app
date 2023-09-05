a666f9924c72c49482aba3bf81809e065421591e
Shri Om Trivedi <tshriom@gmail.com>
2023-09-06 00:12:45 +0530

    create offer is completed

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+let APP_ID = 'c94610628ba94ba999f46be3736384d8';
+
+// token is used only in production, so it will be null for dev.
+let token = null;
+
+// Generate random UID
+let uid = String(Math.floor(Math.random() * 10000));
+let client;
+
+// Channel that two users will join
+let channel;
+
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -11,6 +23,25 @@ const servers = {
 };
 
 let init = async () => {
+  client = await AgoraRTM.createInstance(APP_ID);
+  await client.login({ uid, token });
+
+  // index.html?room=231289
+  channel = client.createChannel('main'); // TODO: replace 'main' with roomID
+  await channel.join();
+
+  // whenever user calls join method we can trigger 'MemberJoined' event listener.
+  channel.on('MemberJoined', handleMemberJoined);
+
+  // triggered when we recieve message from peer.
+  client.on('MessageFromPeer', (message, memberId) => {
+    try {
+      handleMessageFromPeer(message, memberId);
+    } catch (err) {
+      console.log('err: ', err);
+    }
+  });
+
   // This will get your media data
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
@@ -19,33 +50,75 @@ let init = async () => {
 
   // user-1 will get his media stream from here
   document.getElementById('user-1').srcObject = localStream;
+};
 
-  createOffer();
+const handleMemberJoined = async (memberId) => {
+  console.log('A member has joned:', memberId);
+
+  // Create offer when member joins!
+  createOffer(memberId);
+};
+
+const handleMessageFromPeer = async (message, memberId) => {
+  message = JSON.parse(message.text);
+
+  console.log('message', message);
 };
 
 /**
  * creates offer for remote user.
  */
-let createOffer = async () => {
+let createOffer = async (memberId) => {
   // creates a new peer connection
   peerConnection = new RTCPeerConnection(servers);
   remoteStream = new MediaStream();
 
   document.getElementById('user-2').srcObject = remoteStream;
 
+  // If we refresh too fast local stream might not get created,
+  // so I've added this extra check!
+  if (!localStream) {
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    document.getElementById('user-1').srcObject = localStream;
+  }
+  
+  // add tracks to peerConnection
   localStream.getTracks().forEach((track) => {
     peerConnection.addTrack(track, localStream);
   });
 
   // Used when remote user add their track
-  peerConnection.ontrack = (event) => {};
+  peerConnection.ontrack = (e) => {
+    // add tracks of remote user to remoteStream
+    e.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  // setLocalDescription will fire this off and make multiple requests to STUN servers
+  // and it will create ICE candidates for us.
+  peerConnection.onicecandidate = async (e) => {
+    // check if ICE candiate is true
+    if (e.candidate)
+      client.sendMessageToPeer(
+        { text: JSON.stringify({ type: 'candidate', candidate: e.candidate }) },
+        memberId
+      );
+  };
 
   // The createOffer() method of the RTCPeerConnection interface initiates the creation of
   // an SDP offer for the purpose of starting a new WebRTC connection to a remote peer.
   // SDP - Session Description Protocol
   let offer = await peerConnection.createOffer();
-  console.log({ peerConnection });
   await peerConnection.setLocalDescription(offer);
+
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: 'offer', offer: offer }) },
+    memberId
+  );
 };
 
 init();
